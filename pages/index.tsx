203c94ab28618b007df3429ed36f1640fa1b1a5c
mondalraj <mondal021002rajib@gmail.com>
2022-03-14 15:57:41 +0530

    navbar added

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,14 @@ import Image from 'next/image'
 
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'next-i18next'
+import { Navbar } from '../components/Navbar'
 
 const Home: NextPage = ({locale}: any) => {
 
   const {t} = useTranslation();
+
+  console.log(locale);
+  
 
   return (
     <div>
@@ -16,6 +20,8 @@ const Home: NextPage = ({locale}: any) => {
         <meta name="description" content="Charter your private jet effortlessly, anywhere, at any time." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Navbar about={t('common:about')} contact={t('common:contact')} blogs={t('common:blogs')} />
 
       <h1>{t('home:welcome_msg')}</h1>
       <h2>{locale}</h2>
@@ -26,7 +32,7 @@ const Home: NextPage = ({locale}: any) => {
 export async function getStaticProps({locale}: any){
   return {
     props: {
-      ...(await serverSideTranslations(locale, ['home'])),
+      ...(await serverSideTranslations(locale, ['home', 'common'])),
     }
   }
 }
